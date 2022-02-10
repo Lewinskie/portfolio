1995e069e58034a078ab152ee10cd4f33685a74d
Lewinskie <wangaralewis@gmail.com>
2022-02-10 08:57:30 +0300

    added emailJs functionality to contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,29 +4,37 @@ import Phone from "../../img/phone.png";
 import Email from "../../img/email.png";
 import Address from "../../img/address.png";
 import { useRef } from "react";
-// import emailjs from "emailjs";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 
 const Contact = () => {
   const formRef = useRef();
+  const [done, setDone] = useState(false);
+  const [fail, setFail] = useState(false);
+  
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // emailjs
-    //   .sendForm(
-    //     "service_1do55gg",
-    //     "template_qpn0fjl",
-    //     formRef.current,
-    //     "user_5DIwTprilqfao6MsYed8j"
-    //   )
-    //   .then(
-    //     (result) => {
-    //       console.log(result.text);
-    //     },
-    //     (error) => {
-    //       console.log(error.text);
-    //     }
-    //   );
+    emailjs
+      .sendForm(
+        "service_1do55gg",
+        "template_qpn0fjl",
+        formRef.current,
+        "user_5DIwTprilqfao6MsYed8j"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setFail(true);
+        }
+      );
+    
   };
+
   return (
     <div className="c">
       <div className="cBg"></div>
@@ -59,6 +67,18 @@ const Contact = () => {
             <input type="text" placeHolder="Email" name="user_email" />
             <textarea name="message" placeholder="Message" rows="5"></textarea>
             <button>Submit</button>
+            {done && (
+              <div className="cSuccess">
+                <h5 className="cSuccessMessage">Message received!</h5>
+              </div>
+            )}
+            {fail && (
+              <div className="cFail">
+                <h5 className="cFailMessage">
+                  Something went wrong. Try again
+                </h5>
+              </div>
+            )}
           </form>
         </div>
       </div>
