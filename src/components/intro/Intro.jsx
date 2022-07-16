89a6c14ee3e1ef52340cc122524dacc794dda765
Lewinskie <wangaralewis@gmail.com>
2022-07-16 20:21:10 +0300

    added new details

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,15 +11,19 @@ const Intro = () => {
           <div className="iTitle">
             <div className="iTitleWrapper">
               <div className="iTitleItem">Web Developer</div>
-              <div className="iTitleItem">UI/UX Designer</div>
+              <div className="iTitleItem">
+                HTML, CSS, JavaScript, ReactJs, NodeJs,
+              </div>
+              <div className="iTitleItem">CCTV & Acess Control Technician</div>
               <div className="iTitleItem">Network Engineer</div>
               <div className="iTitleItem">Writer</div>
               <div className="iTitleItem">Biker</div>
+              <div className="iTitleItem">GraphQL, Apollo and MongoDB</div>
             </div>
           </div>
 
           <div className="iDesc">
-            i design and develop services for customers of all sizes
+            I design and develop services for customers of all sizes
             specializing in creating stylish , modern websites, web services and
             online stores.
           </div>
